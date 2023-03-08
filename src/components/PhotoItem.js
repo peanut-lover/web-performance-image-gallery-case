8861619ae8fc93902b25a95ff69f5b8aa85f837b
peanut-lover <jihoson94@gmail.com>
2023-03-08 13:09:51 +0900

    Optimize layout shift

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -17,11 +17,20 @@ function PhotoItem({ photo: { urls, alt } }) {
   );
 }
 
-const ImageWrap = styled.div``;
+const ImageWrap = styled.div`
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%;
+  aspect-ratio: 16 / 9;
+`;
 
 const Image = styled.img`
+  position: absolute;
   cursor: pointer;
   width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
 `;
 
 export default PhotoItem;
